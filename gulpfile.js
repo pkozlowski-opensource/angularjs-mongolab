8f4317f43399c38943df2ae00ff2186fb8690d58
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2014-07-26 19:19:50 +0200

    build: various build improvements

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,38 @@
 var gulp = require('gulp');
+var jshint = require('gulp-jshint');
 var karma = require('karma').server;
 
-function karmaExit(exitCode) {
-  process.exit(exitCode);
-}
+var PATHS = {
+  src: 'src/**/*.js',
+  test: 'test/**/*.spec.js'
+};
+
 
 var karmaCommonConf = {
-  browsers: process.env.TRAVIS ? ['Firefox']: ['Chrome'],
+  browsers: process.env.TRAVIS ? ['Firefox'] : ['Chrome'],
   frameworks: ['jasmine'],
   files: [
     'bower_components/angular/angular.js',
     'bower_components/angular-mocks/angular-mocks.js',
-    'src/**/*.js',
-    'test/**/*.js'
+    PATHS.src, PATHS.test
   ],
   singleRun: true
 };
 
-gulp.task('test', function () {
-  karma.start(karmaCommonConf, karmaExit);
+gulp.task('lint', function () {
+  gulp.src([PATHS.src, PATHS.test])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .pipe(jshint.reporter('fail'));
 });
+
+gulp.task('tdd', function (done) {
+  karmaCommonConf.singleRun = false;
+  karma.start(karmaCommonConf, done);
+});
+
+gulp.task('test', function (done) {
+  karma.start(karmaCommonConf, done);
+});
+
+gulp.task('default', ['lint', 'test']);
